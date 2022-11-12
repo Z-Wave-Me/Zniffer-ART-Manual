cccf1a7c3fb4f1c8156801d58584a857d717f257
Xel4ek <l-baal@yandex.ru>
2022-11-12 04:17:34 +0300

    remove console log

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ function toggleMenu() {
   element.classList.toggle('open');
 }
 
-
 (function () {
   const aside = document.querySelector('aside');
   let currentAnchor = '';
@@ -12,7 +11,7 @@ function toggleMenu() {
   const hash = location.hash;
   const bodyTop = document.body.getBoundingClientRect().top;
   function changeAnchor() {
-    navList.forEach(el => {
+    navList.forEach((el) => {
       const id = el.hash.substring(1);
       if (id === currentAnchor) {
         el.classList.add('active');
@@ -22,61 +21,62 @@ function toggleMenu() {
       } else {
         el.classList.remove('active');
       }
-    })
+    });
   }
   function getClosest(currentTop) {
     let dist = null;
-    let closest = "";
-    Object.entries(anchorPositions).map(([id, top])=> {
+    let closest = '';
+    Object.entries(anchorPositions).map(([id, top]) => {
       if (dist === null || (0 < currentTop - top && currentTop - top < dist)) {
         dist = currentTop - top;
         closest = id;
       }
-    })
+    });
     // console.log(dist, closest);
     return closest;
   }
-  navList.forEach(el => {
+  navList.forEach((el) => {
     const id = el.hash.substring(1);
     const target = document.getElementById(id);
     if (target) {
       anchorPositions[id] = target.getBoundingClientRect().top - bodyTop;
-      console.log(target.getBoundingClientRect().top - bodyTop);
     }
-    ['touchstart', 'click'].map(type =>
-    el.addEventListener(type, event => {
-      event.preventDefault();
-      if (window.innerWidth <= 768) {
-        aside.classList.remove('open');
-      }
-      if (history.pushState) {
-        history.pushState(null, null, el.hash);
-      }
-      window.scrollTo({
-        behavior: 'smooth',
-        top:
-          (target?.getBoundingClientRect().top -
-            document.body.getBoundingClientRect().top) ?? 0,
+    ['touchstart', 'click'].map((type) =>
+      el.addEventListener(type, (event) => {
+        event.preventDefault();
+        if (window.innerWidth <= 768) {
+          aside.classList.remove('open');
+        }
+        if (history.pushState) {
+          history.pushState(null, null, el.hash);
+        }
+        window.scrollTo({
+          behavior: 'smooth',
+          top:
+            target?.getBoundingClientRect().top -
+              document.body.getBoundingClientRect().top ?? 0,
+        });
       })
-    }))
+    );
     if (el.hash === hash) {
       el.classList.add('active');
       currentAnchor = id;
     }
-  })
+  });
   let ticking = false;
-  ['scroll', 'resize'].map(event => document.addEventListener(event, function(){
-    if (!ticking) {
-      window.requestAnimationFrame(function() {
-
-        const anchor = getClosest(window.scrollY + 10);
-        if (anchor !== currentAnchor) {
-          currentAnchor = anchor;
-          changeAnchor();
-        }
-        ticking = false;
-      });
-      ticking = true;
-    }
-  }));
-})()
+  ['scroll', 'resize'].map((event) =>
+    document.addEventListener(event, function () {
+      if (!ticking) {
+        window.requestAnimationFrame(function () {
+          const anchor = getClosest(window.scrollY + 10);
+          if (anchor !== currentAnchor) {
+            currentAnchor = anchor;
+            changeAnchor();
+          }
+          ticking = false;
+        });
+        ticking = true;
+      }
+    })
+  );
+})();
